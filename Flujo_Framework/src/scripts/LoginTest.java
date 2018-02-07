8008f0226307ab7cea84b85d88c5d1f381df74f1
RevathiMani654 <csepraba15@gmail.com>
2018-02-07 19:04:33 +0530

    Second Commit

--- Flujo_Framework/src/scripts/LoginTest.java
+++ Flujo_Framework/src/scripts/LoginTest.java
@@ -10,6 +10,7 @@ public class LoginTest extends BaseTest
 	@Test(priority=1,groups={"smoke","login"})
 	public void ClickLogin() throws InterruptedException
 	{
+		//This is LoginPage TestNG Class script
 		HomePage h=new HomePage(driver);
 		h.LoginButton();
 		Thread.sleep(4000);
